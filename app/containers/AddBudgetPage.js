3a06b1a72f4b6d0a2d54abe70bdd70d1adc8cdc3
WeRockStar <kotchaphan.m@gmail.com>
2018-07-05 17:38:41 +0700

    Can query up to one month

--- app/containers/AddBudgetPage.js
+++ app/containers/AddBudgetPage.js
@@ -1,25 +1,34 @@
-import React from 'react'
-import Card from '@material-ui/core/Card'
-import CardHeader from '@material-ui/core/CardHeader'
-import CardContent from '@material-ui/core/CardContent'
-import CardActions from '@material-ui/core/CardActions'
-import Button from '@material-ui/core/Button'
-import TextField from '@material-ui/core/TextField'
+import React from "react";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
 
 const AddBudgetPage = props => (
   <Card>
-    <CardHeader title="Add Budget"/>
+    <CardHeader title="Add Budget" />
     <CardContent>
-      <TextField fullWidth={true} id="month" label="Month" onChange={props.handleChange('month')}/>
+      <TextField
+        fullWidth={true}
+        id="month"
+        label="Month"
+        onChange={props.handleChange("month")}
+      />
       <TextField
         fullWidth={true}
         id="amount"
         label="Amount"
-        onChange={props.handleChange('amount')}
+        onChange={props.handleChange("amount")}
       />
     </CardContent>
     <CardActions>
-      <Button variant="contained" color="primary" onClick={() => props.addBudget()}>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => props.addBudget()}
+      >
         Save
       </Button>
     </CardActions>
@@ -30,49 +39,77 @@ const AddBudgetPage = props => (
           <p>{budget.month}</p>
           <p>{budget.amount}</p>
         </React.Fragment>
-      )
+      );
     })}
   </Card>
-)
+);
 
-class Budgets {
+export class Budgets {
   constructor(setState) {
-    this.setState = setState
-    this.budgets = []
-    this.budget = {month: '', amount: 0}
+    this.setState = setState;
+    this.budgets = [];
+    this.budget = { month: "", amount: 0 };
   }
 
   addBudget = () => {
-    this.budgets.push(this.budget)
-    this.setState({})
-    // this.setState({ budgets: this.budgets })
-  }
+    this.budgets.push({...this.budget});
+    this.setState({});
+  };
   handleChange = name => {
     return e => {
-      this.budget[name] = e.target.value
-      this.setState({budget: this.budget})
+      input(name, e.target.value);
+      this.setState({ budget: this.budget });
+    };
+  };
+  input = (name, value) => {
+    this.budget[name] = value;
+  };
+
+  query = (startDate, endDate) => {
+    const month = startDate.format('YYYY-MM')
+    const selectedBudget = this.budgets.filter((value) => {
+      return value.month == month
+    }, this.budgets)[0]
+    
+    console.log(selectedBudget)    
+
+    if (selectedBudget) {
+      let days = endDate.diff(startDate, "days") + 1
+      let amountOfThisPeriod = selectedBudget.amount
+      let daysInPeriod = 31
+      return amountOfThisPeriod/daysInPeriod*days;
     }
-  }
+    return 0;
+  };
 }
 
 class AddBudgetPageContainer extends React.Component {
-  budgets = new Budgets(state => this.setState(state))
+  budgets = new Budgets(state => this.setState(state));
 
   render() {
     return (
       <Card>
-        <CardHeader title="Add Budget"/>
+        <CardHeader title="Add Budget" />
         <CardContent>
-          <TextField fullWidth={true} id="month" label="Month" onChange={e => this.budgets.handleChange('month')(e)}/>
+          <TextField
+            fullWidth={true}
+            id="month"
+            label="Month"
+            onChange={e => this.budgets.handleChange("month")(e)}
+          />
           <TextField
             fullWidth={true}
             id="amount"
             label="Amount"
-            onChange={(e) => this.budgets.handleChange('amount')(e)}
+            onChange={e => this.budgets.handleChange("amount")(e)}
           />
         </CardContent>
         <CardActions>
-          <Button variant="contained" color="primary" onClick={() => this.budgets.addBudget()}>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => this.budgets.addBudget()}
+          >
             Save
           </Button>
         </CardActions>
@@ -83,23 +120,23 @@ class AddBudgetPageContainer extends React.Component {
               <p>{budget.month}</p>
               <p>{budget.amount}</p>
             </React.Fragment>
-          )
+          );
         })}
       </Card>
-    )
+    );
   }
 }
 
 export class Budget {
-  constructor(month = '', amount = 0) {
-    this.amount = amount
-    this.month = month
+  constructor(month = "", amount = 0) {
+    this.amount = amount;
+    this.month = month;
   }
 }
 
-export default AddBudgetPageContainer
+export default AddBudgetPageContainer;
 
-export function add({month, amount}, budgets) {
-  let budget = new Budget(month, amount)
-  return [...budgets, budget]
+export function add({ month, amount }, budgets) {
+  let budget = new Budget(month, amount);
+  return [...budgets, budget];
 }
---
+++ test/components/AddBudgetPageTest.js
@@ -0,0 +1,83 @@
+import { Budget, add, Budgets } from "../../app/containers/AddBudgetPage";
+import { expect } from "chai";
+import moment from "moment";
+describe("budget", () => {
+  it("should have amount and month", () => {
+    const budget = new Budget();
+    expect(budget).to.deep.eq({
+      amount: 0,
+      month: ""
+    });
+  });
+
+  xit("should accept parameter when creating Budget", () => {
+    const budget = new Budget("2018-06", 1000);
+    expect(budget).to.deep.eq({
+      amount: 1000,
+      month: "2018-06"
+    });
+  });
+
+  xit("should add new budget to the list", () => {
+    const budgets = [];
+    const result = add("2018-06", 1000, budgets);
+    expect(result).to.deep.eq([
+      {
+        month: "2018-06",
+        amount: 1000
+      }
+    ]);
+  });
+});
+
+describe("Budgets", () => {
+  let budgets;
+  beforeEach(() => {
+    budgets = new Budgets(() => {});
+  });
+
+  context("query", () => {
+    it("should return 0", () => {
+      const startDate = moment("2018-01-01");
+      const endDate = moment("2016-12-31");
+      const total = budgets.query(startDate, endDate);
+      expect(total).to.eq(0);
+    });
+
+    context("there is 1 budget", () => {
+      beforeEach(() => {
+        addBudget("2018-07", 3100);
+      });
+      it("full month", () => {
+        const startDate = moment("2018-07-01");
+        const endDate = moment("2018-07-31");
+        const total = budgets.query(startDate, endDate);
+        expect(total).to.eq(3100);
+      });
+
+      it("10 days", () => {
+        const startDate = moment("2018-07-01");
+        const endDate = moment("2018-07-10");
+        const total = budgets.query(startDate, endDate);
+        expect(total).to.eq(1000);
+      });
+    });
+    context("there are 2 budgets", () => {
+      beforeEach(() => {
+        addBudget("2018-06", 1500);
+        addBudget("2018-07", 3100);
+      });
+      it("full month", () => {
+        const startDate = moment("2018-07-01");
+        const endDate = moment("2018-07-31");
+        const total = budgets.query(startDate, endDate);
+        expect(total).to.eq(3100);
+      });
+    });
+  });
+  function addBudget(month, amount) {
+    budgets.input("month", month);
+    budgets.input("amount", amount);
+    budgets.addBudget();
+  }
+});
