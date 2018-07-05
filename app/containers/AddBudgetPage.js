3e16cdd88c71abe348977c411fcfda2ac96c3500
juacompe <juacompe@gmail.com>
2018-07-05 11:11:14 +0700

    add lbudget list and display

--- app/containers/AddBudgetPage.js
+++ app/containers/AddBudgetPage.js
@@ -1,35 +1,84 @@
-import React from 'react'
-import Card from '@material-ui/core/Card'
-import CardHeader from '@material-ui/core/CardHeader'
-import CardContent from '@material-ui/core/CardContent'
-import CardActions from '@material-ui/core/CardActions'
-import Button from '@material-ui/core/Button'
-import TextField from '@material-ui/core/TextField'
+import React from "react";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
 
-const AddBudgetPage = (props) => (
-    <Card>
-      <CardHeader title='Add Budget'/>
-      <CardContent>
-        <TextField fullWidth={true} id="month" label="Month" onChange={props.handleChange('month')}/>
-        <TextField fullWidth={true} id="amount" label="Amount" onChange={props.handleChange('amount')} />
-      </CardContent>
-      <CardActions>
-        <Button variant="contained" color="primary" onClick={() => props.addBudget()}>Save</Button>
-      </CardActions>
-    </Card>
-  )
+const AddBudgetPage = props => (
+  <Card>
+    <CardHeader title="Add Budget" />
+    <CardContent>
+      <TextField
+        fullWidth={true}
+        id="month"
+        label="Month"
+        onChange={props.handleChange("month")}
+      />
+      <TextField
+        fullWidth={true}
+        id="amount"
+        label="Amount"
+        onChange={props.handleChange("amount")}
+      />
+    </CardContent>
+    <CardActions>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => props.addBudget()}
+      >
+        Save
+      </Button>
+    </CardActions>
+    
+    {props.budgets.map(budget => {
+      return (
+        <React.Fragment>
+          <p>{budget.month}</p>
+          <p>{budget.amount}</p>
+        </React.Fragment>
+      );
+    })}
+  </Card>
+);
 
 class AddBudgetPageContainer extends React.Component {
+  state = {
+    budgets: [
+      {
+        month: "",
+        amount: ""
+      }
+    ]
+  };
+
   render() {
-    console.log(this.state)
-    return <AddBudgetPage handleChange={this.handleChange}></AddBudgetPage>
+    return (
+      <AddBudgetPage
+        handleChange={this.handleChange}
+        addBudget={this.addBudget}
+        budgets={this.state.budgets}
+      />
+    );
   }
 
-  handleChange = (name) => {
-    return (e) => {
-      this.setState({[name]: e.target.value})
-    }
-  }
+  addBudget = () => {
+    let budget = {
+      amount: this.state.amount,
+      month: this.state.month
+    };
+    this.setState({
+      budgets: [...this.state.budgets, budget]
+    });
+  };
+
+  handleChange = name => {
+    return e => {
+      this.setState({ [name]: e.target.value });
+    };
+  };
 }
 
-export default AddBudgetPageContainer
+export default AddBudgetPageContainer;
